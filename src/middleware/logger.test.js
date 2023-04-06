aa1c5a922d0f5a79a11f404944934e691ef48b4e
Laurel Perkins <lmperkins20@gmail.com>
2023-04-06 14:36:28 -0700

    still need to refactor

--- src/middleware/logger.test.js
+++ src/middleware/logger.test.js
@@ -5,11 +5,19 @@ const logger = require('./logger.js');
 
 describe('Testing the logger', () => {
 	test('Should log req.method and req.path then pass to the next middleware', () => {
-		const req = {method: "test", path: "test"};
-    const res = {};
-    const next = jest.fn();
+
+		const req = {
+			query: {
+				method: "test",
+				path: "test"
+			}
+		};
+
+		const res = {};
+		const next = jest.fn();
+
 		logger(req, res, next)
 
 		expect(next).toHaveBeenCalled();
-	})
-})
+	});
+});
